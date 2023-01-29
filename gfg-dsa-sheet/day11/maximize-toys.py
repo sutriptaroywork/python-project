41695f91b1dd37dbffb6c2d10be5a5d4e4c0b6b3
sutriptaroywork <sutriptaroywork@gmail.com>
2023-01-30 04:05:05 +0530

    gfg dsa sheet day 11 problem 1

---
+++ gfg-dsa-sheet/day11/maximize-toys.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maximize_toys(self, arr, N , K):
+        no_of_toys = 0
+
+        arr.sort()
+
+        for i in range(N):
+            if arr[i] <= K:
+                no_of_toys += 1
+                K -= arr[i]
+
+        return no_of_toys
+
+
+# Driver code
+obj = Solution()
+arr = [1, 12, 5, 111, 200, 1000, 10]
+n = 7
+k = 50
+res = obj.maximize_toys(arr, n, k)
+print(res) 
+
+
+# here we need to find the maximum number of toys we can buy.
+# we have an array of prices of the toys.
+# so we need to add lowest value toys to add maximum number of toys within the "k" amount of money.
+# so we need to sort the array first to get the lowest amounts first.
+# and then we are just checking that if the price is less than or equal to the money, no of toys is getting increased by 1 and total money is getting decreased by the price of the toy.
+
+# here time complexity of the solution will be - O(n log n) and space complexity will be - O(1).
+# as we are sorting the array and then iterating the array, we have a time complexity of sorting - O(n log n) and a time complexity of iterating - O(n). So adding both becomes O(n log n) + O(n) = O(n log n). Because we always ignore the lower time complexity.
+# as we are using variables to store values which are not directly dependent on the size of the array, the space complexity will be O(1).
+
