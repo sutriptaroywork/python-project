725f0a9ffcc5e27430b22f7bae1f9fb2569322ac
sutriptaroywork <sutriptaroywork@gmail.com>
2023-01-31 00:58:26 +0530

    gfg sde sheet day 14 problem 2

---
+++ gfg-dsa-sheet/day14/check-kth-bit-set.py
@@ -0,0 +1,29 @@
+class Solution:
+    def check_kth_bit_set(self, n, k):
+        count = 0
+        while count <= k:
+            if count == k:
+                remainder = n % 2
+                if remainder == 1:
+                    return True
+            n = n // 2
+            count += 1
+
+        return False
+
+# Driver code
+obj = Solution()
+n = 52
+k = 2
+res = obj.check_kth_bit_set(n, k)
+print(res) 
+
+
+# here we need to find the k th positioned binary digit and check that it is 1 or not.
+# so for that, we need to divide the number upto kth times and on kth time, we need to check if the remainder is 1 or not.
+
+# here time complexity of the solution will be - O(k + 1) and space complexity will be - O(1).
+# as we are starting from 0 and iterating till kth times, the time complexity is - O(k + 1).
+# as we are using variable, which has a constant size, the space complexity will be O(1).
+
+# Video Link - https://www.youtube.com/watch?v=ldhT2uVSdUQ
